ec4219d873790599d3a71c7a82ad5e1f82b4e90a
Taron8530 <102309691+Taron8530@users.noreply.github.com>
2022-10-11 20:03:18 +0900

    kakao_login

--- app/src/main/java/com/example/solo_project/Login.java
+++ app/src/main/java/com/example/solo_project/Login.java
@@ -24,11 +24,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
-//import com.kakao.sdk.auth.model.OAuthToken;
-//import com.kakao.sdk.common.KakaoSdk;
-//import com.kakao.sdk.user.UserApiClient;
-//import com.kakao.sdk.user.model.Account;
-//import com.kakao.sdk.user.model.User;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
+import com.kakao.sdk.user.model.User;
 
 
 import androidx.annotation.NonNull;
@@ -39,7 +39,6 @@ import java.security.NoSuchAlgorithmException;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 public class Login extends AppCompatActivity {
     String TAG = "Login Acticity";
     @Override
@@ -92,31 +91,31 @@ public class Login extends AppCompatActivity {
                 public void onClick(View view) {
                     Log.e("getHash",getKeyHash(Login.this));
 
-//                    KakaoSdk.init(Login.this, "8666d84ac4195731f47eea9eae8e7c17");
-//
-//                    if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(Login.this)){
-//                        UserApiClient.getInstance().loginWithKakaoTalk(Login.this,(oAuthToken, error) -> {
-//                            if (error != null) {
-//                                Log.e("tag", "로그인 실패", error);
-//                                Toast myToast = Toast.makeText(getApplicationContext(),"로그인 실패 다시 시도해주세요.".toString(), Toast.LENGTH_SHORT);
-//                                myToast.show();
-//                            } else if (oAuthToken != null) {
-//                                Log.i("tag", "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-//                                Requestme();
-//                            }
-//                            return null;
-//                        });
-//                    }else{
-//                        UserApiClient.getInstance().loginWithKakaoAccount(Login.this, (Token,error)->{
-//                            if(error != null){
-//                                Log.e("tag", "로그인 실패", error);
-//                            }else if(Token != null){
-//                                Log.i("tag", "로그인 성공(토큰) : " + Token.getAccessToken());
-//                                Requestme();
-//                            }
-//                            return null;
-//                        });
-//                    }
+                    KakaoSdk.init(Login.this, "8666d84ac4195731f47eea9eae8e7c17");
+
+                    if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(Login.this)){
+                        UserApiClient.getInstance().loginWithKakaoTalk(Login.this,(oAuthToken, error) -> {
+                            if (error != null) {
+                                Log.e("tag", "로그인 실패", error);
+                                Toast myToast = Toast.makeText(getApplicationContext(),"로그인 실패 다시 시도해주세요.".toString(), Toast.LENGTH_SHORT);
+                                myToast.show();
+                            } else if (oAuthToken != null) {
+                                Log.i("tag", "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+                                Requestme();
+                            }
+                            return null;
+                        });
+                    }else{
+                        UserApiClient.getInstance().loginWithKakaoAccount(Login.this, (Token,error)->{
+                            if(error != null){
+                                Log.e("tag", "로그인 실패", error);
+                            }else if(Token != null){
+                                Log.i("tag", "로그인 성공(토큰) : " + Token.getAccessToken());
+                                Requestme();
+                            }
+                            return null;
+                        });
+                    }
 
                 }
             });
@@ -152,69 +151,69 @@ public class Login extends AppCompatActivity {
         }
         return null;
     }
-//    public void Requestme(){
-//        UserApiClient.getInstance().me((user, meError) -> {
-//            if (meError != null) {
-//                Log.e("tag", "사용자 정보 요청 실패", meError);
-//            } else {
-//                System.out.println("로그인 완료");
-//                Log.e("tag", user.toString());
-//                {
-//                    ApiInterface apiInterface = Apiclient.getApiClient().create(ApiInterface.class);
-//                    Call<Signup_model> call = apiInterface.getLogin(user.getKakaoAccount().getEmail(),"");
-//                    call.enqueue(new Callback<Signup_model>()
-//                    {
-//                        @Override
-//                        public void onResponse(@NonNull Call<Signup_model> call, @NonNull Response<Signup_model> response)
-//                        {
-//                            boolean test = response.isSuccessful();
-//                            Log.e("selectTest","연결 후");
-//                            System.out.println("selectTest"+test);
-//                            if (response.isSuccessful() && response.body() != null)
-//                            {
-//                                String t = response.toString();
-//                                Log.e("selectTest",t);
-//                                String getted_email = response.body().getE_mail();
-//                                String getted_NK = response.body().getNickname();
-//                                String massage = response.body().getResponse();
-//                                String verify_c = response.body().getVerify();
-//                                Log.e("selectTest()", "서버에서 이메일 : " + getted_email + ", 서버에서 받아온 닉네임 : " + getted_NK+"메세지: "+massage+"/인증코드:"+verify_c);
-//                                if(massage.equals("failed")){
-//                                    Intent I = new Intent(Login.this,Signup_prof.class);
-//                                    I.putExtra("email",user.getKakaoAccount().getEmail());
-//                                    I.putExtra("nickname",user.getKakaoAccount().getProfile().getNickname());
-//                                    I.putExtra("Image",user.getKakaoAccount().getProfile().getProfileImageUrl());
-//                                    startActivity(I);
-//                                    finish();
-//                                }else{
-//                                    SharedPreferences sv = getSharedPreferences("user_verify",MODE_PRIVATE);
-//                                    SharedPreferences.Editor E = sv.edit();
-//                                    E.putString("user_verify",verify_c);
-//                                    E.commit();
-//                                    Intent i = new Intent(Login.this,MainActivity.class);
-//                                    startActivity(i);
-//                                    Toast.makeText(getApplicationContext(),"로그인 성공",Toast.LENGTH_SHORT).show();
-//                                    finish();
-//                                }
-//                            }
-//                            else {
-//                                Log.e("selectTest", "연결이 안댐");
-//                            }
-//                        }
-//
-//                        @Override
-//                        public void onFailure(@NonNull Call<Signup_model> call, @NonNull Throwable t)
-//                        {
-//                            Toast.makeText(Login.this,"연결이 원활하지 않아 나중에 다시 시도하십시오",Toast.LENGTH_SHORT).show();
-//                            Log.e("selectTest()", "에러 : " + t.getMessage());
-//                        }
-//                    });
-//                }
-//
-//            }
-//            return null;
-//        });
-//    }
+    public void Requestme(){
+        UserApiClient.getInstance().me((user, meError) -> {
+            if (meError != null) {
+                Log.e("tag", "사용자 정보 요청 실패", meError);
+            } else {
+                System.out.println("로그인 완료");
+                Log.e("tag", user.toString());
+                {
+                    ApiInterface apiInterface = Apiclient.getApiClient().create(ApiInterface.class);
+                    Call<Signup_model> call = apiInterface.getLogin(user.getKakaoAccount().getEmail(),"");
+                    call.enqueue(new Callback<Signup_model>()
+                    {
+                        @Override
+                        public void onResponse(@NonNull Call<Signup_model> call, @NonNull Response<Signup_model> response)
+                        {
+                            boolean test = response.isSuccessful();
+                            Log.e("selectTest","연결 후");
+                            System.out.println("selectTest"+test);
+                            if (response.isSuccessful() && response.body() != null)
+                            {
+                                String t = response.toString();
+                                Log.e("selectTest",t);
+                                String getted_email = response.body().getE_mail();
+                                String getted_NK = response.body().getNickname();
+                                String massage = response.body().getResponse();
+                                String verify_c = response.body().getVerify();
+                                Log.e("selectTest()", "서버에서 이메일 : " + getted_email + ", 서버에서 받아온 닉네임 : " + getted_NK+"메세지: "+massage+"/인증코드:"+verify_c);
+                                if(massage.equals("failed")){
+                                    Intent I = new Intent(Login.this,Signup_prof.class);
+                                    I.putExtra("email",user.getKakaoAccount().getEmail());
+                                    I.putExtra("nickname",user.getKakaoAccount().getProfile().getNickname());
+                                    I.putExtra("Image",user.getKakaoAccount().getProfile().getProfileImageUrl());
+                                    startActivity(I);
+                                    finish();
+                                }else{
+                                    SharedPreferences sv = getSharedPreferences("user_verify",MODE_PRIVATE);
+                                    SharedPreferences.Editor E = sv.edit();
+                                    E.putString("user_verify",verify_c);
+                                    E.commit();
+                                    Intent i = new Intent(Login.this,MainActivity.class);
+                                    startActivity(i);
+                                    Toast.makeText(getApplicationContext(),"로그인 성공",Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                            }
+                            else {
+                                Log.e("selectTest", "연결이 안댐");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(@NonNull Call<Signup_model> call, @NonNull Throwable t)
+                        {
+                            Toast.makeText(Login.this,"연결이 원활하지 않아 나중에 다시 시도하십시오",Toast.LENGTH_SHORT).show();
+                            Log.e("selectTest()", "에러 : " + t.getMessage());
+                        }
+                    });
+                }
+
+            }
+            return null;
+        });
+    }
     public void findPW(){
 //        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 //        View view = LayoutInflater.from(this).inflate(R.layout.findpw, null, false);
